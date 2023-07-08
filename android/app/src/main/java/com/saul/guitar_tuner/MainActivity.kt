6b7c2858896c83a341ff0bab684613b0e3ddeafd
zhuliminl <zhuliminl@gmail.com>
2023-07-08 21:33:00 +0800

    xxxx

--- android/app/src/main/java/com/saul/guitar_tuner/MainActivity.kt
+++ android/app/src/main/java/com/saul/guitar_tuner/MainActivity.kt
@@ -15,6 +15,7 @@ import com.saul.guitar_tuner.ui.theme.Guitar_tunerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             Guitar_tunerTheme {
                 // A surface container using the 'background' color from the theme
